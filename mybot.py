5a33098d31f6b9a526e4afe3765f1f2c16ebd712
Greatest125 <66028078+Greatest125@users.noreply.github.com>
2022-04-11 21:02:43 -0400

    fixed find_element_by_id (deprecated)
    also added chrome driver auto-download

--- mybot.py
+++ mybot.py
@@ -19,13 +19,13 @@ import http.client
 import urllib.parse
 #import ssl
 from pikepdf import Pdf
-
+import chromedriver_autoinstaller
 """
 This code downloads landfill reports from the noted website, cleans them, while retaining a URL to the file in pdf_links.csv.
 The code can be changed to reflect a different site and different button clicks to get to the filtered files desired.
 The code requires a copy of chromedriver.exe, and everything runs within one folder.
 """
-
+chromedriver_autoinstaller.install()  
 #>>>>>>>>>>>>>>>>>>>>> here you asign the starting and the ending page <<<<<<<<<<<<<<<<<<<<<<<
 startPage = 1 #from
 pagelimit =80 #till (check this occassionally to confirm a reasonable maximum to save time)
@@ -190,14 +190,14 @@ def setFirstCall(button_name, viewstate, fileName, currentPage):
   conn = http.client.HTTPSConnection("geos.epd.georgia.gov")
   payload = 'SimpleMainContent_MainContent_tabView_IDX=0&__ASYNCPOST=true&__EVENTARGUMENT=&__EVENTTARGET=&__LASTFOCUS=&__VIEWSTATE='+viewstate+'&__VIEWSTATEGENERATOR=55AD252D&ctl00%24ctl00%24ScriptManager1=ctl00%24ctl00%24SimpleMainContent%24MainContent%24ucApplicationSubmitList%24UpdatePanel2%7C'+button_name+'&'+button_name+'.x=17&'+button_name+'.y=6&ctl00%24ctl00%24SimpleMainContent%24MainContent%24ucApplicationSubmitList%24ddlApplication=2148&ctl00%24ctl00%24SimpleMainContent%24MainContent%24ucApplicationSubmitList%24ddlApplicationType=10036&ctl00%24ctl00%24SimpleMainContent%24MainContent%24ucApplicationSubmitList%24ddlCategory=1&ctl00%24ctl00%24SimpleMainContent%24MainContent%24ucApplicationSubmitList%24ddlProgram=3&ctl00%24ctl00%24SimpleMainContent%24MainContent%24ucApplicationSubmitList%24ddlSiteCounty=&ctl00%24ctl00%24SimpleMainContent%24MainContent%24ucApplicationSubmitList%24ddlSubmissionStatus=&ctl00%24ctl00%24SimpleMainContent%24MainContent%24ucApplicationSubmitList%24txtEndDate=&ctl00%24ctl00%24SimpleMainContent%24MainContent%24ucApplicationSubmitList%24txtFacilityName=&ctl00%24ctl00%24SimpleMainContent%24MainContent%24ucApplicationSubmitList%24txtPermitNumber=&ctl00%24ctl00%24SimpleMainContent%24MainContent%24ucApplicationSubmitList%24txtSiteAddress1=&ctl00%24ctl00%24SimpleMainContent%24MainContent%24ucApplicationSubmitList%24txtSiteCity=&ctl00%24ctl00%24SimpleMainContent%24MainContent%24ucApplicationSubmitList%24txtStartDate=&ctl00%24ctl00%24SimpleMainContent%24MainContent%24ucApplicationSubmitList%24txtSubmissionId=&ctl00%24ctl00%24footerContent%24Footer1%24ctl00%24PopupPanel1eftPopPanlHF=false&ctl00%24ctl00%24footerContent%24Footer1%24ctl00%24pnleftPopPanlHF=false&ctl00%24ctl00%24sDateFormat=&hiddenInputToUpdateATBuffer_CommonToolkitScripts=1'
   headers = {
-    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
+    'sec-ch-ua': '"Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
     'sec-ch-ua-mobile': '?0',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36',
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
     'Cache-Control': 'no-cache',
     'X-Requested-With': 'XMLHttpRequest',
-    'X-MicrosoftAjax': 'Delta=true',
-    'sec-ch-ua-platform': '"Windows"',
+#   'X-MicrosoftAjax': 'Delta=true',
+    'sec-ch-ua-platform': '"Linux"',
     'Accept': '*/*',
     'Cookie': 'ASP.NET_SessionId=lyo0az01basqynsstsckdkew; BIGipServereft-http-pool-GOV-PRD-GA-GEOS=1470671020.20480.0000'
   }
@@ -216,12 +216,10 @@ def setFirstCall(button_name, viewstate, fileName, currentPage):
   #next API call
   repayload = ''
   reheaders = {
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
+    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
     'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
+    'sec-ch-ua-platform': '"Linux"',
     'Cookie': 'ASP.NET_SessionId=lyo0az01basqynsstsckdkew; BIGipServereft-http-pool-GOV-PRD-GA-GEOS=1470671020.20480.0000'
   }
   try:
@@ -248,28 +246,28 @@ def setFirstCall(button_name, viewstate, fileName, currentPage):
   getPDF(docLink, fileName)
 
 def filterSelections():
-  chrome_options = webdriver.ChromeOptions()
-  chrome_options.add_argument("--incognito")
-  driver = webdriver.Chrome(chrome_options=chrome_options)
+  options = webdriver.ChromeOptions()
+  options.add_argument("--incognito")
+  driver = webdriver.Chrome(options=options)
   driver.get(baseurl+"/GA/GEOS/Public/Client/GA_GEOS/Public/Pages/PublicApplicationList.aspx")
 
   print(">>Selecting Category<<")
-  select_cat = Select(driver.find_element_by_id('SimpleMainContent_MainContent_ucApplicationSubmitList_ddlCategory')) 
+  select_cat = Select(driver.find_element(By.ID,'SimpleMainContent_MainContent_ucApplicationSubmitList_ddlCategory')) 
   select_cat.select_by_value('1')
   waitTillLoader(driver)
 
   print(">>Selecting Program<<")
-  select_pro = Select(driver.find_element_by_id('SimpleMainContent_MainContent_ucApplicationSubmitList_ddlProgram')) 
+  select_pro = Select(driver.find_element(By.ID, 'SimpleMainContent_MainContent_ucApplicationSubmitList_ddlProgram')) 
   select_pro.select_by_value('3')
   waitTillLoader(driver)
 
   print(">>Selecting App-type<<")
-  select_app_type = Select(driver.find_element_by_id('SimpleMainContent_MainContent_ucApplicationSubmitList_ddlApplicationType')) 
+  select_app_type = Select(driver.find_element(By.ID, 'SimpleMainContent_MainContent_ucApplicationSubmitList_ddlApplicationType')) 
   select_app_type.select_by_value('10036')
   waitTillLoader(driver)
 
   print(">>Selecting App<<")
-  select_app = Select(driver.find_element_by_id('SimpleMainContent_MainContent_ucApplicationSubmitList_ddlApplication')) 
+  select_app = Select(driver.find_element(By.ID,'SimpleMainContent_MainContent_ucApplicationSubmitList_ddlApplication')) 
   select_app.select_by_value('2148')
   waitTillLoader(driver)
 
